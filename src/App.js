1f3b30d86231a8c85ab897e49547ac4d494c61d0
shecho <shechovala@gmail.com>
2020-12-07 19:28:59 -0500

    added publication logint to het and fetch data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  let [publications, setPublications] = useState([]);
+
+  useEffect(() => {
+    getPublications();
+  }, []);
+  const getPublications = async () => {
+    let url = "http://127.0.0.1:8000/publications/";
+    let response = await fetch(url);
+    let res = await response.json();
+    setPublications((prev) => res);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        {publications.map((publication) => {
+          return (
+            <div className="col-md-4 m-2 text-center" key={publication.author}>
+              <div className="card bg-dark text-light">
+                <div className="card-body">
+                  <h5 className="card-title"> {publication.content}</h5>
+                  <p> {publication.author}</p>
+                  <p> {publication.likes}</p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
